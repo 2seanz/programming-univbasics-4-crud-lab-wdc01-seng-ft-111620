7148e3b5bf254766a530eadc46d1f29ae4196455
confident-collaborator-2097 <seanzngo@gmail.com>
2020-10-29 18:04:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   sick_movies = ["taxi driver", "batman", "mad max", "the raid"]
-  sick_movies<<"pulp fiction"
+  sick_movies<<"pulp_fiction"
 end
 
 def add_element_to_start_of_array(array, element)
